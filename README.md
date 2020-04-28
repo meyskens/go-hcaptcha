a1b8f7fc846c1e4e6aaa36cfb2e03334bda35654
Maartje Eyskens <maartje@eyskens.me>
2020-04-28 11:17:28 +0200

    Initial code
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

---
+++ README.md
@@ -0,0 +1,24 @@
+# hCaptcha Go library
+
+This library is a [hCaptcha](https://www.hcaptcha.com/) server side library for Go.
+It allows to verify challange responses sent to the server.
+
+## Usage
+The library gives a struct set up to verify the challenges of a given private key set up with `New()`, responses received by the server can be verified with `Verify()`.
+```go
+import "github.com/meyskens/go-hcaptcha"
+
+func handleInvite(w http.ResponseWriter, r *http.Request) {
+    hcaptchaResponse, _ := r.Form["h-captcha-response"]
+	hc := hcaptcha.New("<insert secret key>")
+    //Get IP from RemoteAddr
+    ip, _, err := net.SplitHostPort(r.RemoteAddr)
+
+    
+    resp, err := hc.Verify(hcaptchaResponse, ip)
+    // handle errors please!
+    if resp.Success {
+        // captcha OK!
+    }
+}
+```
---
+++ hcapctha.go
@@ -0,0 +1,60 @@
+package hcaptcha
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type HCaptcha struct {
+	Secret      string
+	HCaptchaURL string
+}
+
+type Response struct {
+	// Success indicates if the challenge was passed
+	Success bool `json:"success"`
+	// ChallengeTs is the timestamp of the captcha
+	ChallengeTs time.Time `json:"challenge_ts"`
+	// Hostname is the hostname of the passed captcha
+	Hostname string `json:"hostname"`
+	// Credit indicates  whether the response will be credited (optional)
+	Credit bool `json:"credit"`
+	// ErrorCodes contains error codes returned by hCaptcha (optional)
+	ErrorCodes []string `json:"error-codes"`
+}
+
+func New(secret string) *HCaptcha {
+	return &HCaptcha{
+		Secret:      secret,
+		HCaptchaURL: "https://hcaptcha.com/siteverify",
+	}
+}
+
+// Verify verifies a "h-captcha-response" data field, with an optional remote IP set.
+func (h *HCaptcha) Verify(response, remoteip string) (*Response, error) {
+	values := url.Values{"secret": {h.Secret}, "response": {response}}
+	if remoteip != "" {
+		values.Set("remoteip", remoteip.String())
+	}
+	resp, err := http.PostForm(h.HCaptchaURL, values)
+	if err != nil {
+		return nil, fmt.Errorf("HTTP error: %w", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("HTTP read error: %w", err)
+	}
+
+	r := Response{}
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return nil, fmt.Errorf("JSON error: %w", err)
+	}
+
+	return &r, nil
+}
